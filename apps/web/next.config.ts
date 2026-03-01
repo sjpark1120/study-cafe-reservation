77aff43c88ee0f41f1e780ca8561cf82f7ddda32
sjpark1120 <asgol9609@gmail.com>
2026-03-02 00:27:37 +0900

    feat(config): 로컬 IP를 허용하는 이미지 설정 추가

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,19 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        dangerouslyAllowLocalIP: true,
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '3001',
+                pathname: '/cafe-images/**',
+            },
+        ],
+    },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
